15e103a27eacefee336f3d780ce64c55c91ff801
YulianDeLosSantos26 <yuliandelossantoss26@gmail.com>
2025-07-03 10:40:54 -0400

    continuando

---
+++ operadores/06-orden.js
@@ -0,0 +1,17 @@
+
+// Tiene que dar valor 16
+
+let resultado = 8/2*(2+2); 
+
+// Tiene que dar valor 1
+
+let resultado1 = 8/(2*(2+2)); // dato extra para seleccionar el orden en que quiero que se ejecute una operacion lo encierro entre () y con eso se le asigna el orden.
+
+console.log(resultado);
+console.log(resultado1);
+
+/* Hay que tener en cuenta que el orden den las operaciones matematicas es importante,
+y que si aparece dentro de parentesis algo, hay que ponerle un * de multiplicacion para 
+que haga la operacion dentro del parentesis ().
+
+*/
---
+++ operadores/07-ternario.js
@@ -0,0 +1,15 @@
+// Expresion ? 'Si es true' : 'si es false'
+
+/*
+Operador ternario: es un operador condicional que permite evaluar, una expresión basada en una condición
+ y devolver un valor u otro dependiendo de si la condición es verdadera o falsa, es lo mismo que 
+ expresar estructuras if-else simples en una sola línea. En esencia, toma tres operandos: una condición, 
+ un valor si la condición es verdadera, y un valor si la condición es falsa. 
+
+*/
+
+let edad = 17;
+
+let acceso = edad >= 17 ? 'Permitir ingreso ' : 'No puede ingresar';
+
+console.log(acceso)
